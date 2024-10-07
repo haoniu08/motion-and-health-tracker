31e986e29a9d468e51ae81865563dbf14c077744
haoniu08 <niu.hao@northeastern.edu>
2024-10-07 10:52:11 -0700

    bottom tabs added, needed folders arranged

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import Activities from './screens/Activities';
+import Diet from './screens/Diet';
+import Settings  from './screens/Settings';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Activities" component={Activities} />
+        <Tab.Screen name="Diet" component={Diet} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 

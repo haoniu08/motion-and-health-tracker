6c65177ed1eb320d0d9a88746b9fe5adb5736a41
haoniu08 <niu.hao@northeastern.edu>
2024-10-08 18:53:34 -0700

    modifying bottom tab icons

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 import Styles from './utils/StylingUtils';
 import Activities from './screens/Activities';
 import Diet from './screens/Diet';
@@ -14,7 +15,23 @@ const Stack = createStackNavigator();
 
 function BottomTabs () {
   return (
-    <BottomTab.Navigator>
+    <BottomTab.Navigator
+      screenOptions = {({route}) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Activities') {
+            iconName = focused ? "walk" : "walk-outline";
+          } else if (route.name === 'Diet') {
+            iconName = focused ? 'fast-food' : 'fast-food-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <BottomTab.Screen name="Activities" component={Activities} />
       <BottomTab.Screen name="Diet" component={Diet} />
       <BottomTab.Screen name="Settings" component={Settings} />
